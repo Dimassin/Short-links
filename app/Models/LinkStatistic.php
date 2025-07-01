625be4be1befd1ea0a16f1718ee06ab93bae94df
dima_maximov_git <dmitry.maksimov@izex.ru>
2025-07-01 21:51:12 +0300

    git init

---
+++ app/Models/LinkStatistic.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class LinkStatistic extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public $timestamps = false;
+    protected $fillable = ['link_id', 'ip_address', 'created_at'];
+
+    public function link(): BelongsTo
+    {
+        return $this->belongsTo(Link::class);
+    }
+}
---
+++ database/migrations/2025_07_01_181756_create_links_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('original_url', 1000);
+            $table->string('short_url')->unique()->index();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('links');
+    }
+};
